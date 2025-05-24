b5f95524d95c26bbf56fc15453be127bfe26516d
Mehrinoz <abdullayevamehrinoz845@gmail.com>
2025-05-24 22:25:48 +0500

    yangilash

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'saytim1.wsgi'
--- taalim/settings.py
+++ taalim/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,10 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / 'mainapp' / 'static']
-# STATIC_ROOT =BASE_DIR/'staticfiles'
-STATICFILES_DIRS =('/home/mehrinoz/mehrinoznews.uz/students/static',)
-STATIC_ROOT='/home/mehrinoz/mehrinoznews.uz/students/staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'mainapp' / 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR/'staticfiles')
+# STATICFILES_DIRS =('/home/mehrinoz/mehrinoznews.uz/students/static',)
+# STATIC_ROOT='/home/mehrinoz/mehrinoznews.uz/students/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -122,5 +122,5 @@ LOGIN_REDIRECT_URL = '/'
 
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR/'media'
-MEDIA_ROOT ='/home/mehrinoz/mehrinoznews.uz/students/media'
+MEDIA_ROOT = os.path.join(BASE_DIR/'media')
+# MEDIA_ROOT ='/home/mehrinoz/mehrinoznews.uz/students/media'
